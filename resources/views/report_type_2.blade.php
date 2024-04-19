2edce7fbd1a89762724821fc6e593a4a4519446f
ForSureN1 <standin1532@gmail.com>
2024-04-19 14:39:15 +0500

    pdf

--- resources/views/report_type_2.blade.php
+++ resources/views/report_type_2.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Отчет по заказу 2</title>
     <style>
