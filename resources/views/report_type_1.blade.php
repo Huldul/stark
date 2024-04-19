f36f04a07f2eb9d329f07a2bcd113c6b9f3ada90
ForSureN1 <standin1532@gmail.com>
2024-04-19 14:34:13 +0500

    pdf

--- resources/views/report_type_1.blade.php
+++ resources/views/report_type_1.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Отчет по заказу 1</title>
     <style>
-        @font-face {
+        /* @font-face {
             font-family: 'DejaVu Sans';
             font-style: normal;
             font-weight: 400;
             src: url({{ storage_path('fonts/DejaVuSans.ttf') }}) format('truetype');
-        }
+        } */
         body {
-            /* font-family: sans-serif; */
+            font-family: unset;
         }
         .header {
             text-align: center;
