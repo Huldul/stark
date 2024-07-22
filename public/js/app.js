f47d9c9082be4ef454c5db1008888446306ddd75
Miron Fissenko <mirfees@gmail.com>
2024-07-22 19:30:16 +0500

    Update index.blad.php

--- public/js/app.js
+++ public/js/app.js
@@ -347,7 +347,7 @@ window.onload = () => {
     });
     const heroSlider = new Swiper ('.hero__slider', {
         autoplay: true,
-        effect: 'fade',
+        // effect: 'fade',
         loop: true,
         slidesPerView: 1,
         spaceBetween: 20,
